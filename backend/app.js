f71c88bbfc5450492b812b84ccf8117befa4ed9a
Vinicius <vine.goncalo123@gmail.com>
2025-11-10 22:40:46 -0300

    criação do router / controller do registro do usuario

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,21 @@
 import express from 'express';
+import userRouter from './src/router/userRouter.js';
+
 
 const app = express();
+
+//porta do servidor
 const PORT = 3000;
 
+//rota do usuario
+app.use (express.json ());
+app.use ('/usuarios', userRouter);
 app.get('/', (req, res) => {
     res.json(arrResponse);   
-
-
 })
 
+//inicia o servidor
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// middleware para interpretar dados de formulários html
+app.use (express.urlencoded({ etended: true }));
--- backend/src/controller/userController.js
+++ backend/src/controller/userController.js
@@ -1,5 +1,6 @@
-import * as User from '../model/userModel.js'; // esta importando todas as funçoes do arquivo mencionado.
+import * as User from '../models/userModels.js'; // esta importando todas as funçoes do arquivo mencionado.
 
+//guscar todos os usuarios
 export const getUsers = async (req, res) => {
     try {
         const users = await User.getAllUsers ();
@@ -9,3 +10,50 @@ export const getUsers = async (req, res) => {
 
     }
 };
+
+
+//criar usuario
+export const createUser = async (req, res) =>{
+    const {nome, email, senha_hash, confirmar_senha} = req.body;
+
+    try{
+        if (senha_hash !== confirmar_senha){
+            return res.status (400).json ({error: "As senhas nao conferem"})
+        }
+
+        const newUser = await User.createUser (nome, email, senha_hash);
+        if (!updateUser) return res.status (404).json ({error: "Usuario nao encontrado"});
+        res.json (updateUser);
+    } catch (err) {
+        console.error (err);
+        res.status (500).json ({error: "Erro ao criar usuario"});
+
+    }
+};
+
+//atualizar usuario
+export const updateUser = async (req, res) => {
+    const {id} = req.params;
+    const {nome, email, senha_hash} = req.body;
+    try {
+        const updateUser = await User.updateUser (id, nome, email, senha_hash);
+        if (!updateUser) return res.status (404).json ({error: "Usuario nao encontrado"});
+        res.json (updateUser);
+    } catch (err) {
+        console.error (err);
+        res.status (500).json ({error: "Erro ao atualizar usuario"});
+    }
+};
+
+//deletar usuario
+export const deleteUser = async (req,res) => {
+    const {id} = req.params;
+    try{
+        await User.deleteUser (id);
+        res.json ({message: "Usuario deletado com sucesso"});
+    } catch (err) {
+        console.error (err);
+        res.status (500).json ({error: "Erro ao deletar usuario"});
+    }
+};
+
